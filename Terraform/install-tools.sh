9126c33abae8d000a11d5ef39cc5bcc91b0e5568
MariaSoto <sotomaria2304@gmail.com>
2025-06-26 21:43:27 -0400

    updating files

--- Terraform/install-tools.sh
+++ Terraform/install-tools.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-# It installs all the necessary tools for the K8s assignment.
-
 # Use -e to exit immediately if a command fails
 set -e
 
@@ -20,15 +17,40 @@ usermod -aG docker ec2-user
 
 # --- Install kubectl ---
 echo "Installing kubectl..."
-# Download a temporary location and then move it to a directory in the PATH
-curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.5/2024-01-04/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-mv ./kubectl /usr/local/bin/kubectl
+curl -o /usr/local/bin/kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.5/2024-01-04/bin/linux/amd64/kubectl
+chmod +x /usr/local/bin/kubectl
 
 # --- Install kind ---
 echo "Installing kind..."
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
-chmod +x ./kind
-mv ./kind /usr/local/bin/kind
+curl -Lo /usr/local/bin/kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x /usr/local/bin/kind
 
-echo "All prerequisites installed successfully!"
+# --- Configure ECR Credential Helper ---
+echo "Installing and configuring ECR credential helper..."
+yum install -y amazon-ecr-credential-helper
+
+# Create the Docker config for the ec2-user to automatically use the helper
+# This ensures that when you SSH in, your user is already configured.
+mkdir -p /home/ec2-user/.docker
+cat <<EOF > /home/ec2-user/.docker/config.json
+{
+    "credHelpers": {
+        "public.ecr.aws": "ecr-login",
+        "396561719560.dkr.ecr.us-east-1.amazonaws.com": "ecr-login"
+    }
+}
+EOF
+# Set the correct ownership for the config file
+chown -R ec2-user:ec2-user /home/ec2-user/.docker
+
+# --- Configure cgroups v2 for Kind ---
+echo "Enabling cgroups v2..."
+grubby --args="systemd.unified_cgroup_hierarchy=1" --update-kernel DEFAULT
+
+echo "All prerequisites installed and system configured."
+echo "Instance will now reboot to apply cgroups v2 setting."
+
+# --- Reboot the instance ---
+# The reboot is necessary for the cgroup setting to take effect.
+# The User Data script will not run again after the reboot.
+reboot
